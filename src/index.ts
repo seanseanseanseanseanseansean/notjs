3128555aa31ffa40797efc61ff225bc05dca0961
sean <126618424+seanseanseanseanseanseansean@users.noreply.github.com>
2023-02-28 21:49:34 +0100

    aggiornata la funzione per entrare in una singola riga

--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,4 @@
 * @param { boolean } expression the boolean value
 * @returns { boolean } the opposite of the value
 */
-export function not(expression: boolean): boolean {
-    if (expression == true) {
-        return false
-    }
-    else if (expression == false) {
-        return true
-    }
-    else {
-        throw new TypeError("Invalid Expression!")
-    }
-}
+export function not(expression: boolean) return !expression;
